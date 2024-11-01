c05ab7b4e312f28c7d462a8623821aa69ce4ce7a
jose-calderon1996 <joseignaciocalderonroldan39@gmail.com>
2024-10-31 21:55:46 -0300

    escanner

---
+++ src/app/scanner-qr/scanner-qr.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scanner-qr',
+  templateUrl: './scanner-qr.page.html',
+  styleUrls: ['./scanner-qr.page.scss'],
+})
+export class ScannerQRPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
