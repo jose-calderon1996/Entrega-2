82f19e0030bc7ae0b49218e6e9c4338cbc0df21a
jose-calderon1996 <joseignaciocalderonroldan39@gmail.com>
2024-11-04 18:17:36 -0300

    Remove node_modules from tracking

--- node_modules/@capacitor-firebase/firestore/android/src/main/java/io/capawesome/capacitorjs/plugins/firebase/firestore/interfaces/QueryFilterConstraint.java
+++
@@ -1,9 +0,0 @@
-package io.capawesome.capacitorjs.plugins.firebase.firestore.interfaces;
-
-import com.google.firebase.firestore.Filter;
-import javax.annotation.Nullable;
-
-public interface QueryFilterConstraint {
-    @Nullable
-    Filter toFilter();
-}
